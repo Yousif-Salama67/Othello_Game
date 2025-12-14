5cb5abf3711c286bce53211ce2e79154db1f7398
NancyAlaa1 <nancy3laa2005@gmail.com>
2025-12-14 22:42:04 +0200

    My_Functions

---
+++ othello.py
@@ -0,0 +1,51 @@
+# =========== Intialization The Board And Starting Positions ===========
+    def _init_(self):
+        self.size = 8
+        self.board = [[" " for _ in range(self.size)] for _ in range(self.size)]
+        self.board[3][3] = 'W'
+        self.board[3][4] = 'B'
+        self.board[4][3] = 'B'
+        self.board[4][4] = 'W'
+
+        self.current_player = "B"
+
+#------------------------------------------------------------------------------------------------
+
+     # =========== Switch The Current Player ===========
+    def switch_player(self):
+        self.current_player = "W" if self.current_player == "B" else "B"
+
+#-----------------------------------------------------------------------------------------------
+
+    # =========== Check If The Move Is Valid ===========
+    def is_valid_move(self, player, row, col, board):
+        if board[row][col] != " ":
+            return False
+        opponent = "W" if player == "B" else "B"
+        directions = [(-1,0),(1,0),(0,-1),(0,1),(-1,-1),(1,-1),(-1,1),(1,1)]
+        for dr, dc in directions:
+            r, c = row + dr, col + dc
+            found_opponent = False
+            while 0 <= r < self.size and 0 <= c < self.size:
+                if board[r][c] == opponent:
+                    found_opponent = True
+                    r += dr
+                    c += dc
+                elif board[r][c] == player:
+                    if found_opponent:
+                        return True
+                    break
+                else:
+                    break
+        return False
+
+#-------------------------------------------------------------------------------------------------
+
+    # ===========  Return A list Of All Valid Moves For A Player Represented As A Tuple ===========
+    def get_valid_moves(self, player, board):
+        moves = []
+        for r in range(self.size):
+            for c in range(self.size):
+                if self.is_valid_move(player, r, c, board):
+                    moves.append((r, c))
+        return moves
